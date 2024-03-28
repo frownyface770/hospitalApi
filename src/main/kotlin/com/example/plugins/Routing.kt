c034ef8328bb7bdbed339e6b31eed625936cd9f2
ricardofpmartins <164074791+ricardofpmartins@users.noreply.github.com>
2024-03-28 20:00:50 +0000

    Create routes for doctors and their respective database

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import com.example.routes.PatientRoutes
+import com.example.routes.DoctorRoutes
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -9,5 +10,8 @@ fun Application.configureRouting() {
     routing {
        val patientRoutes = PatientRoutes()
         patientRoutes.setUpPatientRoutes(this)
+       val doctorRoutes = DoctorRoutes()
+        doctorRoutes.setUpDoctorRoutes(this)
+
     }
 }
