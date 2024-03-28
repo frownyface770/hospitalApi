f300e884da0520d3f564b9f35cb875057f451772
MiguelSilva <miguelsilva.martins@ua.pt>
2024-03-28 20:17:10 +0000

    Creating appoint db service and routes Change the models and classes as needed.

--- src/main/kotlin/com/example/models/Appointment.kt
+++ src/main/kotlin/com/example/models/Appointment.kt
@@ -1,5 +1,81 @@
 package com.example.models
 
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.Date
 
-data class Appointment(val id: String, val date: Date, val patient: Patient, val doctor: Doctor)
+class Appointment(
+    val id: Int,
+    val date: String,
+    val patientID: Int,
+    val doctorID: Int,
+    val patientComments: String)
+
+class AppointmentDB {
+    init {
+        transaction {
+            SchemaUtils.create(Appointments)
+        }
+    }
+
+
+    fun getAppointmentByPatient(id: Int):List<Appointment> {
+        try {
+            return transaction {
+                Appointments.select { Appointments.patientID eq id}.map {rowToAppointment(it)}
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return emptyList()
+        }
+    }
+
+    fun addAppointment(appointment: Appointment): Boolean {
+        try {
+            transaction {
+                Appointments.insert {
+                    it[date] = appointment.date
+                    it[patientID] = appointment.patientID
+                    it[doctorID] = appointment.doctorID
+                    it[patientComments] = appointment.patientComments
+                }
+            }
+            return true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
+    }
+
+    private fun rowToAppointment(row: ResultRow): Appointment {
+        return Appointment(
+            id = row[Appointments.id],
+            date = row[Appointments.date],
+            patientID = row[Appointments.patientID],
+            doctorID = row[Appointments.doctorID],
+            patientComments = row[Appointments.patientComments]
+        )
+    }
+}
+
+class AppointmentService(private val appointmentDB: AppointmentDB) {
+    fun getApointments(id:Int): List<Appointment> {
+        return appointmentDB.getAppointmentByPatient(id)
+    }
+
+    fun addAppointment(newAppointment: Appointment): Boolean {
+        return appointmentDB.addAppointment(newAppointment)
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/kotlin/com/example/routes/AppointmentRoutes.kt
@@ -0,0 +1,57 @@
+package com.example.routes
+
+import com.example.models.*
+import io.ktor.server.application.*
+import io.ktor.http.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+class AppointmentRoutes {
+    private val appointmentDB = AppointmentDB()
+
+    private val appointmentService = AppointmentService(appointmentDB)
+
+    fun setUpAppointmentRoutes(route: Route) {
+        route.route("/appointment") {
+            listAppointments()
+            createAppointment()
+        }
+    }
+
+    private fun Route.createAppointment() {
+        post("/createAppointment") {
+            val appointment = call.receive<Appointment>()
+
+            try {
+                val success = appointmentService.addAppointment(appointment)
+
+                if (success) {
+                    call.respondText("Appointment stored correctly", status = HttpStatusCode.Created)
+                } else {
+                    call.respondText("Appointment already exists", status= HttpStatusCode.Conflict)
+                }
+            } catch (e: Exception) {
+                call.respondText("Error creating appointment ${e.message}", status = HttpStatusCode.InternalServerError)
+            }
+        }
+    }
+    private fun Route.listAppointments() {
+        get("/{id}") {
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                "Bad request"
+            )
+            try {
+                val appointmentStorage = appointmentService.getApointments(id.toInt())
+                if (appointmentStorage.isNotEmpty()) {
+                    call.respond(appointmentStorage)
+                } else {
+                    call.respondText("No appointments found", status = HttpStatusCode.OK)
+                }
+            } catch (e: Exception) {
+                call.respondText("Error retrieving appointments ${e.message}", status = HttpStatusCode.InternalServerError)
+            }
+        }
+    }
+
+}
