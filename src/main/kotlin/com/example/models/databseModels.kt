0dea464337d27b8f441cea621849203cf6913c9e
ricardofpmartins <164074791+ricardofpmartins@users.noreply.github.com>
2024-03-28 20:04:10 +0000

    Revert "Create routes for doctors and their respective database"
    This reverts commit c034ef8328bb7bdbed339e6b31eed625936cd9f2.

--- src/main/kotlin/com/example/models/databseModels.kt
+++ src/main/kotlin/com/example/models/databseModels.kt
@@ -11,18 +11,5 @@ object Patients : Table() {
 
     //Overides the primaryKey and names it for clarity's sake
     override val primaryKey = PrimaryKey(id,name ="PK_Patients_ID")
-}
 
-object Doctors : Table() {
-    val id = integer("id").autoIncrement()
-    val firstName = varchar("first_name", length=100)
-    val lastName = varchar("last_name", length=100)
-    val age = integer("age")
-    val email = varchar("email", length = 255).nullable()
-    var gender = enumeration("gender", Gender::class)
-    var dateOfBirth = varchar("date_of_birth", length=20)
-    var department = varchar("department", length=20)
-
-    //Overides the primaryKey and names it for clarity's sake
-    override val primaryKey = PrimaryKey(id,name ="PK_Doctor_ID")
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,7 +1,6 @@
 package com.example.plugins
 
 import com.example.routes.PatientRoutes
-import com.example.routes.DoctorRoutes
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -10,8 +9,5 @@ fun Application.configureRouting() {
     routing {
        val patientRoutes = PatientRoutes()
         patientRoutes.setUpPatientRoutes(this)
-       val doctorRoutes = DoctorRoutes()
-        doctorRoutes.setUpDoctorRoutes(this)
-
     }
 }
