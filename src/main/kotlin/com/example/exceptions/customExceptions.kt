66a38dfb73972e556c2f2a4bd121052ff160a117
MiguelSilva <miguelsilva.martins@ua.pt>
2024-05-11 18:41:31 +0100

    Added update appointment info at all lvls

--- src/main/kotlin/com/example/exceptions/customExceptions.kt
+++ src/main/kotlin/com/example/exceptions/customExceptions.kt
@@ -3,4 +3,7 @@ package com.example.exceptions
 class PatientAlreadyExistsException(id: String) : Exception("Patient with $id already exists")
 class PatientNotFoundException(id: String) : Exception("Patient with $id doesn't exist")
 
+
+class AppointmentAlreadyExistsException() : Exception("An appointment for that day, time and doctor already exists.")
+class AppointmentNotFoundException(id:String): Exception("Appoint with $id id not found")
 class ServiceException(message: String?, val originalException: Exception) : Exception(message)
--- src/main/kotlin/com/example/models/Appointment.kt
+++ src/main/kotlin/com/example/models/Appointment.kt
@@ -1,5 +1,6 @@
 package com.example.models
 
+import com.example.exceptions.*
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -8,11 +9,11 @@ import java.util.Date
 @Serializable
 class Appointment(
     val id: Int = 1,
-    val date: String,
-    val time: String,
-    val patientID: Int,
-    val doctorID: Int,
-    val patientComments: String)
+    var date: String,
+    var time: String,
+    var patientID: Int,
+    var doctorID: Int,
+    var patientComments: String)
 
 class AppointmentDB {
     init {
@@ -31,9 +32,19 @@ class AppointmentDB {
             return emptyList()
         }
     }
+    fun getAppointmentById(id: Int): Appointment? {
+        try {
+            return transaction {
+                Appointments.select { Appointments.id eq id }.map { rowToAppointment(it) }.firstOrNull()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw e
 
+        }
+    }
 
-    fun addAppointment(appointment: Appointment): Boolean {
+    fun addAppointment(appointment: Appointment) {
         try {
             transaction {
                 Appointments.insert {
@@ -44,20 +55,35 @@ class AppointmentDB {
                     it[patientComments] = appointment.patientComments
                 }
             }
-            return true
+
         } catch (e: Exception) {
             e.printStackTrace()
             //We have a unique index in the database table of the date, time and doc id, if we try to insert another
             // appointment with those same details and exception is thrown. That message contains this exact message.
             // So we catch it and return a more meaningful message to the user.
             if (e.message?.contains("appointments_date_time_doctorid_unique") == true) {
-                throw Exception("Appointment already exists at that time for the given doctor. Choose another time or doctor")
+                throw AppointmentAlreadyExistsException()
             }
-            throw Exception("Refer to the documentation for more information.")
+            throw e
            // return false
         }
     }
-
+     fun updateAppointment(updatedAppointment: Appointment) {
+        try {
+            transaction {
+                Appointments.update({ Appointments.id eq updatedAppointment.id }) {
+                    it[date] = updatedAppointment.date
+                    it[time] = updatedAppointment.time
+                    it[patientID] = updatedAppointment.patientID
+                    it[doctorID] = updatedAppointment.doctorID
+                    it[patientComments] = updatedAppointment.patientComments
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw e
+        }
+    }
 
 
     private fun rowToAppointment(row: ResultRow): Appointment {
@@ -77,9 +103,21 @@ class AppointmentService(private val appointmentDB: AppointmentDB) {
         return appointmentDB.getAppointmentByPatient(id)
     }
 
-    fun addAppointment(newAppointment: Appointment): Boolean {
+    fun addAppointment(newAppointment: Appointment) {
+        appointmentDB.addAppointment(newAppointment)
+    }
 
-        return appointmentDB.addAppointment(newAppointment)
+    fun updateAppointment(id:String,updatedAppointment: Appointment) {
+        val existingAppointment = appointmentDB.getAppointmentById(id.toInt()) ?: throw AppointmentNotFoundException(id)
+        existingAppointment.apply {
+            existingAppointment.date = updatedAppointment.date
+            existingAppointment.time = updatedAppointment.date
+            existingAppointment.doctorID = updatedAppointment.doctorID
+            existingAppointment.patientID = updatedAppointment.patientID
+            existingAppointment.patientComments = updatedAppointment.patientComments
+        }
+        appointmentDB.updateAppointment(updatedAppointment)
+
     }
 }
 
