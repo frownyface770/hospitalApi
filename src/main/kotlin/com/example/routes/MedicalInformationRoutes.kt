25758952cef51a5b446ade581dddce3364d05508
joaovalenteacabou <150954368+joaovalenteacabou@users.noreply.github.com>
2024-05-23 21:23:05 +0100

    Start making routes for medicalinformation
    start of the making of the routes for the medicalinformation

--- src/main/kotlin/com/example/routes/MedicalInformationRoutes.kt
+++ src/main/kotlin/com/example/routes/MedicalInformationRoutes.kt
@@ -7,13 +7,23 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 class MedicalInformationRoutes {
-    //private val MedicalInformationDB = MedicalInformations
-    //private val MedicalInformationService = MedicalInformationService(MedicalInformationDB)
+    private val medicalInformationDB = MedicalInformationDB()
+    private val medicalInformationService = MedicalInformationService(medicalInformationDB)
     fun setUpMedicalInformationRoutes(route: Route) {
         route.route("/medicalinformation") {
             //Calls the functions that actually handle the routing
 
         }
     }
+     private fun Route.createMedicalInformation() {
+         post("/medicalinformation") {
+             val medicalInformation = call.receive<MedicalInformation>()
+             try {
+                 medicalInformationService.addMedicalInformation(medicalInformation)
+                 call.respondText("Medical information stored corretly", status = HttpStatusCode.Created)
+             } catch(e: Exception) {
+                 call.respondText("Error creating medical information ${e.message}", status = HttpStatusCode.InternalServerError)
+             }
+         }
 
-}
+     }     }
